6eacf11cdb7aa6ff643a5209156d0b134695051b
Garima Mishra <98249934+Gar12ima@users.noreply.github.com>
2025-05-27 12:06:51 +0530

    Add files via upload

---
+++ Connection.py
@@ -0,0 +1,9 @@
+import mysql.connector 
+class conn: 
+mydb = mysql.connector.connect( 
+host="localhost", 
+user="root", 
+passwd="1234", 
+database="carparking_python" 
+) 
+mycursor = mydb.cursor()
---
+++ welcome_suport.py
@@ -0,0 +1,50 @@
+import sys 
+from conn import conn 
+import easygui 
+try: 
+import Tkinter as tk 
+except ImportError: 
+import tkinter as tk 
+try: 
+import ttk 
+py3 = False 
+except ImportError: 
+import tkinter.ttk as ttk 
+py3 = True 
+def carpark(a,b,c,d): 
+ct = conn() 
+sql = "INSERT INTO carpark VALUES ('" + a + "', '" + b + "','" + c + "', 
+'" + d + "')" 
+ct.mycursor.execute(sql) 
+ct.mydb.commit() 
+# --------------------------------------------------------------------- 
+sql = "UPDATE carsloat SET photo='1.JPG' WHERE IDd = '" + d + "'" 
+ct.mycursor.execute(sql) 
+ct.mydb.commit() 
+# print(ct.mycursor.rowcount, "record Update ") 
+a = easygui.msgbox("Record Update.", title="BOX") 
+root.destroy() 
+# --------------------------------------------------------------------- 
+root.destroy() 
+# import start 
+# start.vp_start_gui() 
+#sys.stdout.flush() 
+def viewcar(): 
+print("hh") 
+import CarParking_new 
+CarParking_new.parksloat() 
+sys.stdout.flush() 
+def init(top, gui, *args, **kwargs): 
+global w, top_level, root 
+w = gui 
+top_level = top 
+root = top 
+def destroy_window(): 
+print("dd") 
+# Function which closes the window. 
+global top_level 
+top_level.destroy() 
+top_level = None 
+if __name__ == '__main__': 
+import welcome 
+welcome.vp_start_gui() 
